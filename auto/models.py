af2788248fc6924a936a966569e09bd2c8fe72b3
mohammadrezarouhani <mohammadrezarouhani@protonmail.com>
2022-11-17 11:33:17 +0330

    all models created with their relationship

--- auto/models.py
+++ auto/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import os,pathlib,uuid,random,string
+
 
 class BaseUser(AbstractUser):
-    departman=models.OneToOneField('Departman',on_delete=models.SET_NULL)
-    image=models.ImageField(default='def.jpg',upload_to='user_photo')
+    email=models.EmailField(unique=True)
+    # image=models.ImageField(default='def.jpg',upload_to='user_photo')
+    is_staff=models.BooleanField(default=True)
 
     USERNAME_FIELD='email'
     REQUIRED_FIELDS=['username']
@@ -12,14 +15,44 @@ class BaseUser(AbstractUser):
 class Departman(models.Model):
     title=models.CharField(max_length=255)
     discription=models.TextField(null=True,blank=True)
+    user=models.ForeignKey(BaseUser,on_delete=models.CASCADE)
 
 
-class Letter(models.Model):
+class Ticket(models.Model):
+    PRIORITY=[('H','HIGH'),('M','MIDIUM'),('L','LOW')]
+    HIGH=('H','HIGH')
+    MEDIUM=('M','MIDIUM')
+    LOW=('L','LOW')
+
     title=models.CharField(max_length=255)
     discription=models.TextField()
-    reciever=models.ForeignKey(Departman,oo_delete=models.CASCADE)
-    images=models.ImageField()
+    priority=models.CharField(max_length=115,choices=PRIORITY,default=MEDIUM)
+    sender=models.ForeignKey(BaseUser,on_delete=models.CASCADE,related_name='sender')
+    reciever=models.ForeignKey(BaseUser,on_delete=models.CASCADE,related_name='reciever')
+    departman=models.ForeignKey(Departman,on_delete=models.SET_NULL,null=True,blank=True)
+    created_at=models.DateTimeField(auto_now=True)
+    updated_at=models.DateTimeField(null=True,blank=True)
 
 
-class Image(models.Model):
-    pass
+class TicketMessage(models.Model):
+    letter=models.ForeignKey(Ticket,on_delete=models.CASCADE)
+    title=models.CharField(max_length=255)
+    discription=models.TextField()
+    create_at=models.DateTimeField(auto_now=True)
+
+
+class TicketHistory(models.Model):
+    ticket=models.ForeignKey(Ticket,on_delete=models.CASCADE)
+    owner=models.ForeignKey(BaseUser,on_delete=models.CASCADE)
+    departman=models.ForeignKey(Departman,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now=True)
+
+
+def get_image_path(model,filename):
+    return os.path.join('content_file',str(uuid.uuid4())+pathlib.Path(filename).suffix)
+
+class FileUpload(models.Model):
+    letter=models.ForeignKey(TicketMessage,on_delete=models.CASCADE)
+    image=models.FileField(upload_to=get_image_path)
+    created_at=models.DateTimeField(auto_now=True)
+
